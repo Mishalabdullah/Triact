4632b3c1f475cec24cbc5bdd40870e5a2a748127
Mishalabdullah <github.ijvob@simplelogin.co>
2023-04-10 13:35:18 +0530

    added links

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,11 +3,13 @@ function Portfolioitem({ title, imgUrl, stack, link }) {
   return (
     <div className="border-2 border-stone-900 rounded-md overflow-hidden  m-2">
       <div>
-        <img
-          src={imgUrl}
-          alt="image not found"
-          className="w-full h-36 md:h-48 object-cover cursor-pointer"
-        />
+        <a href={link} target="blank">
+          <img
+            src={imgUrl}
+            alt="image not found"
+            className="w-full h-36 md:h-48 object-cover cursor-pointer"
+          />
+        </a>
       </div>
       <div className="w-full p-4">
         <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
